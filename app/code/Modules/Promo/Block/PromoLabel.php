485eaaf8cf904fb1f44f07ba5f0830a0d11d2fe5
PonasVoras8 <debesysmelyni@gmail.com>
2019-08-06 16:26:51 +0300

    Promo label polishing

--- app/code/Modules/Promo/Block/PromoLabel.php
+++ app/code/Modules/Promo/Block/PromoLabel.php
@@ -4,34 +4,29 @@ use DateTime;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Modules\Promo\Helper\Data;
+use Magento\Catalog\Model\Product;
 //use Magento\Catalog\Helper\Data;
 
 class PromoLabel extends Template
 {
     private $helper;
-
+    private $product;
 
     public function __construct(
+        Product $product,
         Context $context,
         Data $helper)
     {
+        $this->product = $product;
         $this->helper = $helper;
         parent::__construct($context);
     }
 
-    public function checkAdminPromotion()
+    public function checkPromotion()
     {
-        return $this->helper->getGeneralConfig('enable');
-    }
-
-    public function checkItemPromotion()
-    {
+        $this->helper->getGeneralConfig('enable');
+        var_dump($this->product->getData('PromoEnable_attribute'));
         return 1;
-    }
-
-    public function checkPrice()
-    {
-        return 4.20;
     }
 
     public function getLabelText()
--- app/code/Modules/Promo/Plugins/Promo.php
+++ app/code/Modules/Promo/Plugins/Promo.php
@@ -6,8 +6,22 @@ class Promo
 {
     public function aftergetPrice(Product $product, $price)
     {
-        $price = $product->getData('price');
-        //var_dump($price);
-        return $price;
+//        $price = $product->getData('price');
+//        $checkPromo = $product->getData('PromoEnable_attribute');
+////        if ($checkPromo !== 0) {
+////            $price = $product->getData('PromoPrice_attribute');
+//////            var_dump($price);
+//////            var_dump($product->getData('PromoPrice_attribute'));
+//////            var_dump($product->getData('PromoEnable_attribute'));
+////        } else {
+////            $price = $product->getData('price');
+////        }
+//        return $price;
+
+        if (!$product->getData('PromoEnable_attribute')) {
+            return $price;
+        }
+        return floatval($product->getData('PromoPrice_attribute'));
     }
+    //}
 }
