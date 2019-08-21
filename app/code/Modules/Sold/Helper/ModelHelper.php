7b69772fd8a95bb7a55aa564f82c7ecf0ecab87c
PonasVoras8 <debesysmelyni@gmail.com>
2019-08-21 14:24:55 +0300

    Refactoring

--- app/code/Modules/Sold/Helper/ModelHelper.php
+++ app/code/Modules/Sold/Helper/ModelHelper.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Modules\Sold\Helper;
 
 use Modules\Sold\Model\ResourceModel\OrderedResourceModelFactory;
@@ -9,72 +8,60 @@ class ModelHelper
 {
     private $logger;
     private $orderedResourceModelFactory;
-    private $orderedCollection;
+    private $orderModelFactory;
+    private $orderModel;
     private $orderedCollectionFactory;
     private $orderedResourceModel;
-
     public function __construct(
+        OrderModelFactory $orderModelFactory,
         OrderedCollectionFactory $orderedCollectionFactory,
         OrderedResourceModelFactory $orderedResourceModelFactory,
         LoggerHelper $logger
     ) {
         $this->logger = $logger;
+        $this->orderModelFactory = $orderModelFactory;
         $this->orderedCollectionFactory = $orderedCollectionFactory;
         $this->orderedResourceModelFactory = $orderedResourceModelFactory;
         $this->orderedResourceModel = $this->orderedResourceModelFactory->create();
+        $this->orderModel = $this->orderModelFactory->create();
     }
-
     public function resetOrderedCount(string $sku)
     {
-        //$configurableSku = $this->getConfigurableSku($sku);
-//        if (!empty($configurableSku)) {
-//            $this->orderModelAddQuantity($configurableSku);
-//        }
+        $configurableSku = $this->getConfigurableSku($sku);
+        if (!empty($configurableSku)) {
+            $this->orderModelAddQuantity($configurableSku);
+        }
         $this->orderModelAddQuantity($sku);
     }
-
     public function orderModelAddQuantity(string $sku, bool $increment = false): void
     {
-        $orderedCollection = $this->orderedCollectionFactory->create();
-//        $orderedCollection = $orderedCollection->addFieldToFilter('*', array(
-//            'like' => $sku . ' %'
-//        ));
-        $orderedCollection = $orderedCollection->addFieldToSelect('*')->load();
-        //$this->logger->logIfEnabled(json_encode($orderedCollection));
-
-        foreach ($orderedCollection as $product) {
-            $this->logger->logIfEnabled(json_encode($product));
+        $quantity = null;
+        if ($increment) {
+            $quantity = $this->orderModel
+                ->load($sku, 'sku')
+                ->getData('sold_quantity');
+            $quantity++;
         }
 
-//        $quantity = null;
-//        if ($increment) {
-//            $quantity = $this->orderModel
-//                ->load($sku, 'sku')
-//                ->getData('sold_quantity');
-//            $quantity++;
-//        }
-//
-//        $newQuantity = $this->orderModel
-//            ->load($sku, 'sku')
-//            ->addData([
-//                'sold_quantity' => $quantity
-//            ]);
-//        $savedNewQuantity = $newQuantity->save();
-//
-//        if ($savedNewQuantity) {
-//            $this->logger->logIfEnabled('Quantity ' . $sku . ' saved successfully');
-//        }
-    }
+        $newQuantity = $this->orderModel
+            ->load($sku, 'sku')
+            ->addData([
+                'sold_quantity' => $quantity
+            ]);
+        $savedNewQuantity = $newQuantity->save();
 
+        if ($savedNewQuantity) {
+            $this->logger->logIfEnabled('Quantity ' . $sku . ' saved successfully');
+        }
+    }
     public function handleOrderedItem(string $sku)
     {
         $this->orderModelAddQuantity($sku, true);
-        //$configurableSku = $this->getConfigurableSku($sku);
-//        if (!empty($configurableSku)) {
-//            $this->orderModelAddQuantity($configurableSku, true);
-//        }
+        $configurableSku = $this->getConfigurableSku($sku);
+        if (!empty($configurableSku)) {
+            $this->orderModelAddQuantity($configurableSku, true);
+        }
     }
-
     public function getConfigurableSku(string $sku): string
     {
         $configurableSku = explode('-', $sku);
